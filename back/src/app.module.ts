c44d290dd1dee6d9f206fa4c48642eac078a6f13
NicolasAlx <nicolas1.alexandre@epitech.eu>
2025-02-01 14:18:10 +0100

    feat: dependencies have been update on the package.json

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaServiceModule } from './microservices/prisma-service/prisma-service.module';
+import { PrismaController } from './microservices/prisma-service/prisma/prisma.controller';
+import { TasksServiceModule } from './microservices/tasks-service/tasks-service.module';
+import { ListServiceModule } from './microservices/lists-service/lists-service.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PrismaServiceModule, TasksServiceModule, ListServiceModule],
+  controllers: [PrismaController],
   providers: [AppService],
 })
 export class AppModule {}
